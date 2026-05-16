8b1829d7e7ca6b8d0074732f32b071fec4fdcbbd
duongductrong <duongductrong06@gmail.com>
2026-05-17 01:22:59 +0700

    Initial Commit

---
+++ Ghosted/GhostedApp.swift
@@ -0,0 +1,32 @@
+//
+//  GhostedApp.swift
+//  Ghosted
+//
+//  Created by duongductrong on 17/5/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct GhostedApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
